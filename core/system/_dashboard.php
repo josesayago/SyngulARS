cc54b9215a35f413301b4d5af59651f16b5ea9a5
José SAYAGO <jose.sayago@laelite.info>
2015-04-23 23:51:58 -0430

    Step 1
    Building first step.

--- core/system/_dashboard.php
+++ core/system/_dashboard.php
@@ -7,10 +7,19 @@
 Class SyngulARS_Dashboard {
 	
 	public function _dashboard() {
-		$title 	= __('Welcome to the WordPress Theme Builder', 'syngulars');
+		$title 		= __('Welcome to the WordPress Theme Builder', 'syngulars');
+		$title_S1 	= __('Name your Theme and Pick a Base Layout', 'syngulars');
+		
 		$html 	= '<div class="syngulars_dashboard">';
-		$html  .= '<h1>'.$title.'</h1>';
-		$html  .= '</div>';
+			$html  	.= '<h1>'.$title.'</h1>';
+
+			$html 	.= '<div class="step-1">';
+
+				$html 	.= '<h2>'.$title_S1.'</h2>';
+
+			$html 	.= '</div>';
+
+		$html  	.= '</div>';
 
 		print $html;
 	}
