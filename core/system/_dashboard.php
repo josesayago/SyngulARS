b86dd81115cb8b196e7d26051501eb8b660c1db3
José SAYAGO <jose.sayago@laelite.info>
2015-04-24 13:36:36 -0430

    Dashboard
    Added template thumbnails, first step 75% finished.

--- core/system/_dashboard.php
+++ core/system/_dashboard.php
@@ -30,17 +30,59 @@ Class SyngulARS_Dashboard {
 
 					$html 	.= '<p class="italic">'.__('Your base layout will be','syngulars').':</p>';
 
-					$html 	.= '<input type="radio" name="{{template_type}}" value="landing-page" /><span class="radio-label">'.__('Landing Page','syngulars').'</span>';
+					$html 	.= '<input type="radio" name="template_type" value="landing-page" /><span class="radio-label">'.__('Landing Page','syngulars').'</span>';
 					
-					$html 	.= '<input type="radio" name="{{template_type}}" value="post" /><span class="radio-label">'.__('Post','syngulars').'</span>';
+					$html 	.= '<input type="radio" name="template_type" value="post" /><span class="radio-label">'.__('Post','syngulars').'</span>';
 					
-					$html 	.= '<input type="radio" name="{{template_type}}" value="page" /><span class="radio-label">'.__('Page','syngulars').'</span>';
+					$html 	.= '<input type="radio" name="template_type" value="page" /><span class="radio-label">'.__('Page','syngulars').'</span>';
 
 					$html 	.= '<div class="clearfix">&nbsp;</div>';
 
 					$html 	.= '<p class="italic">'.__('TIP: These base layouts can be completely customized, this is just a quick start.','syngulars').'</p>';
 
 					$html 	.= '<div class="clearfix">&nbsp;</div>';
+
+				$html 	.= '</div>';
+
+				$html 	.= '<div class="content-templates">';
+
+					$html 	.= '<div class="template-model">';
+
+						$html 	.= '<img src="'.SYNGULARS_ASSETS.'images/template-1.png" />';
+
+						$html 	.= '<div class="clearfix">&nbsp;</div>';
+
+						$html 	.= '<input type="radio" name="template_style" value="page" /><span class="radio-label">'.__('Blog','syngulars').'</span>';
+
+					$html 	.= '</div>';
+
+					$html 	.= '<div class="template-model">';
+
+						$html 	.= '<img src="'.SYNGULARS_ASSETS.'images/template-2.png" />';
+
+						$html 	.= '<div class="clearfix">&nbsp;</div>';
+
+						$html 	.= '<input type="radio" name="template_style" value="page" /><span class="radio-label">'.__('Portfolio','syngulars').'</span>';
+
+					$html 	.= '</div>';
+
+					$html 	.= '<div class="template-model">';
+
+						$html 	.= '<img src="'.SYNGULARS_ASSETS.'images/template-3.png" />';
+
+						$html 	.= '<div class="clearfix">&nbsp;</div>';
+
+						$html 	.= '<input type="radio" name="template_style" value="page" /><span class="radio-label">'.__('Call to Action','syngulars').'</span>';
+
+					$html 	.= '</div>';
+
+					$html 	.= '<div class="step-btns">';
+
+						$html 	.= '<a class="btn-left inactive">'.__('Previous Step','syngulars').'</a>';
+
+						$html 	.= '<a class="btn-right">'.__('Next Step', 'syngulars').'</a>';
+
+					$html 	.= '</div>';
 
 				$html 	.= '</div>';
 
